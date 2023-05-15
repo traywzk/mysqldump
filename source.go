bcfedfe470bab986fe49aef969ff9be2eb89b409
traywzk <470496680@qq.com>
2023-05-15 18:04:42 +0800

    错误的sql输出

--- source.go
+++ source.go
@@ -173,6 +173,7 @@ func Source(dns string, reader io.Reader, opts ...SourceOption) error {
 
 		_, err = dbWrapper.Exec(ssql)
 		if err != nil {
+			log.Printf("[sqk] %v\n", ssql)
 			log.Printf("[error] %v\n", err)
 			//执行失败不中断
 			//return err
